a747944090f5be0f8095fc16049c9cb3c19b993a
Ramsey2022 <abovethelotus@yahoo.com>
2023-08-11 13:00:40 -0700

    added likes and notification models

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,6 @@
 class Notification < ApplicationRecord
+  belongs_to :notifier, class_name: 'User'
+  belongs_to :actor, class_name: 'User'
+  belongs_to :comment, optional: true
+  belongs_to :like, optional: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_10_235820) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_11_194555) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_10_235820) do
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "notifications", force: :cascade do |t|
@@ -65,6 +74,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_10_235820) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "notifications", "users", column: "actor_id"
   add_foreign_key "notifications", "users", column: "notifier_id"
   add_foreign_key "posts", "users", column: "author_id"
